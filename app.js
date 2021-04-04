6dca9edd6e826b7daf6fa20c60c4fee7ed6d958a
Nemeh998 <nemh998@gmail.com>
2021-04-04 13:57:30 +0300

    update html page

---
+++ app.js
@@ -0,0 +1,10 @@
+
+for(var i=0; i<5 ;i++){
+
+
+  console.log(i);
+document.write('<img src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/43D8/production/_111686371_pic1.png"/>')
+
+
+}
+console.log(i);
