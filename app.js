54949038ceb8fa87f1edbb42e9a5df956ee6ee0b
Nemeh998 <nemh998@gmail.com>
2021-04-05 15:42:26 +0300

    add function

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 
 
-var userinp=prompt('enter page name (html)or(css)');
-while(userinp!=='html'&& userinp!=='css'){
+function mylanguges(){
+    var userinp=prompt('enter page name (html)or(css)');
+    while(userinp!=='html'&& userinp!=='css'){
 
-userinp=prompt('enter page name (html)or(css)');
+        userinp=prompt('enter page name (html)or(css)');
 
+
+    }
 }
 
-
+function showImag(){
     var pic=prompt('pleas enter number to show Image');
 
 
@@ -16,23 +19,28 @@ for(var i=0;i<pic;i++){
 document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/HTML_logo.png"/>');
 document.write(i);
 }
+}
 
-// var ma = prompt('pleas enter a number','100');
-//   alert(ma +100);
+function vale(){
+var ma = prompt('pleas enter a number','100');
+  alert(ma +100);
 
-// console.log(ma*3);
+console.log(ma*3);
 
 
 
-//  if(ma <0){
-//   alert(" you input negative vale");
-// }
+ if(ma <0){
+  alert(" you input negative vale");
+}
   
-// else if (ma >=0){
-//   alert(" you input positive vale");
-// }
-// else  {
-//   alert(" please enter a number");
-// }
+else if (ma >=0){
+  alert(" you input positive vale");
+}
+else  {
+  alert(" please enter a number");
+}
+}
 
-
+mylanguges();
+showImag();
+vale();
